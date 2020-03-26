c37632d3bc1a5a27fc1801cf7a68bcfabf49381e
sujkowskidawid <sujkowskidawid942gmail.com>
2020-03-26 11:14:34 +0100

    test

---
+++ src/main/java/Apartaments.java
@@ -0,0 +1,20 @@
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+public class Apartaments {
+
+    private String city;
+
+    @ToString.Include(name = "m^2")
+    private double area;
+
+    @ToString.Exclude
+    private double price;
+
+
+    public double getFullPrice(){
+        return area*price*0.95;
+    }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Apartaments apartament1 = new Apartaments();
+        Apartaments apartament2 = new Apartaments();
+        Apartaments apartament3 = new Apartaments();
+
+        apartament1.setCity("Warsaw");
+        apartament1.setArea(100);
+        apartament1.setPrice(8000);
+
+        apartament2.setCity("Poznan");
+        apartament2.setArea(82);
+        apartament2.setPrice(6400);
+
+        apartament3.setCity("Gdansk");
+        apartament3.setArea(75);
+        apartament3.setPrice(7350);
+
+        ArrayList<Apartaments> apartamentsList = new ArrayList<>();
+
+        apartamentsList.add(apartament1);
+        apartamentsList.add(apartament2);
+        apartamentsList.add(apartament3);
+
+        for (Apartaments apartaments : apartamentsList) {
+            System.out.println(apartaments);
+            System.out.println("Całkowita cena mieszkania to:" + apartaments.getFullPrice());
+        }
+
+        double średniaCena = (apartament1.getFullPrice()+apartament2.getFullPrice()+apartament3.getFullPrice())/apartamentsList.size();
+        System.out.format("Mean price is: %.2f" , średniaCena);
+
+    }
+
+}
