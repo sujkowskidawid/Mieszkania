270a9ed42dea1b2a69754f2ae9471bb43b249fcc
Marcin Smoliga <marcinsmoliga88@gmail.com>
2020-03-26 20:27:50 +0100

    created House and ApartmentBuilding Object

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,38 +3,63 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Apartaments apartament1 = new Apartaments();
-        Apartaments apartament2 = new Apartaments();
-        Apartaments apartament3 = new Apartaments();
+        //TEST
+        House house= new House();
+        house.setNumberOfRooms(6);
+        house.setNumberOfWindows(15);
+        house.setArea(150);
+        house.setHasGarden(true);
+        house.setHasGarage(true);
+        house.setHasGym(false);
+        house.setOneStorey(true);
 
-        apartament1.setCity("Warsaw");
-        apartament1.setArea(100);
-        apartament1.setPrice(8000);
+        ApartmentBuilding apartmentBuilding = new ApartmentBuilding();
+        apartmentBuilding.setNumberOfRooms(80);
+        apartmentBuilding.setNumberOfWindows(120);
+        apartmentBuilding.setArea(2_300);
+        apartmentBuilding.setHasGarden(false);
+        apartmentBuilding.setHasGarage(false);
+        apartmentBuilding.setHasGym(true);
+        apartmentBuilding.setNumberOfApartments(40);
+        apartmentBuilding.setNumberOfFloors(4);
 
-        apartament2.setCity("Poznan");
-        apartament2.setArea(82);
-        apartament2.setPrice(6400);
+        System.out.println(house);
+        System.out.println(apartmentBuilding);
+        System.out.println();
+        // TEST END
 
-        apartament3.setCity("Gdansk");
-        apartament3.setArea(75);
-        apartament3.setPrice(7350);
+        Apartment apartment1 = new Apartment();
+        Apartment apartment2 = new Apartment();
+        Apartment apartment3 = new Apartment();
 
-        ArrayList<Apartaments> apartamentsList = new ArrayList<>();
+        apartment1.setCity("Warsaw");
+        apartment1.setArea(100);
+        apartment1.setPrice(8000);
 
-        apartamentsList.add(apartament1);
-        apartamentsList.add(apartament2);
-        apartamentsList.add(apartament3);
+        apartment2.setCity("Poznan");
+        apartment2.setArea(82);
+        apartment2.setPrice(6400);
 
-        for (Apartaments apartaments : apartamentsList) {
+        apartment3.setCity("Gdansk");
+        apartment3.setArea(75);
+        apartment3.setPrice(7350);
+
+        ArrayList<Apartment> apartmentsList = new ArrayList<>();
+
+        apartmentsList.add(apartment1);
+        apartmentsList.add(apartment2);
+        apartmentsList.add(apartment3);
+
+        for (Apartment apartaments : apartmentsList) {
             System.out.println(apartaments);
             System.out.println("Całkowita cena mieszkania to:" + apartaments.getFullPrice() + "\n");
 
         }
         double sum = 0;
-        for (Apartaments a : apartamentsList) {
+        for (Apartment a : apartmentsList) {
             sum += a.getFullPrice();
         }
-        System.out.format("średnia cena mieszkań to: %.2f", sum / apartamentsList.size());
+        System.out.format("średnia cena mieszkań to: %.2f", sum / apartmentsList.size());
 
 
     }
